08df3803d02828a1660460735a1d95cafa18ce35
차동현 <121537101+chadonghyun@users.noreply.github.com>
2023-03-23 18:02:55 +0900

    IP01 ver 1.2

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,19 @@
-function onGeoOk(){
+const API_KEY = '87a88927cf765d0b901a963b77472ef4';
 
+
+function onGeoOk(position){
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  fetch(url).then(Response => Response.json()).then(data => {
+    const weather = document.querySelector('#weather span:first-child')
+    const city = document.querySelector('#weather span:last-child')
+    weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    city.innerText = data.name;
+  });
 }
-function onError(){
-  
+function onGeoError(){
+  alert("Can't find you. No weather for you.")
 }
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
